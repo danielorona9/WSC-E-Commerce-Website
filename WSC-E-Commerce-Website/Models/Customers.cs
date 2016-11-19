037b8bf973ab8a3626a4c7d8ffd9f6b85cef5474
danielorona9 <dorona8@my.devry.edu>
2016-11-19 14:28:19 -0800

    Customer class added with columns version 1

---
+++ WSC-E-Commerce-Website/Models/Customers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WSC_E_Commerce_Website.Models
+{
+    public class Customers
+    {
+        public int CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public char State { get; set; }
+        public int Zip { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "0:MM-dd-yyyy", ApplyFormatInEditMode = true)]
+        [Display(Name = "Registered Date")]
+        public DateTime RegisteredDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "0:MM-dd-yyyy", ApplyFormatInEditMode = true)]
+        [Display(Name = "Modified Date")]
+        public DateTime ModifiedDate { get; set; }
+
+        public bool Locked { get; set; }
+    }
+}
