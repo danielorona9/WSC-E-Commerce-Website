c99fa071b0e18f67b01aac5399e5bb7bf6a6a28a
danielorona9 <dorona@my.devry.edu>
2016-11-20 23:55:58 -0800

    Added credit card type class

---
+++ WSC-E-Commerce-Website/Models/CreditCardType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WSC_E_Commerce_Website.Models
+{
+    public class CreditCardType
+    {
+        public int CreditCardTypeID { get; set;}
+
+        [Required(ErrorMessage = "please select a credit card type")]
+        [Display(Name = "Credit Card Type")]
+        public string CreditCardTypeName { get; set; }
+
+
+        public virtual ICollection<BillingInfo> BillingInfo { get; set; }
+    }
+}
