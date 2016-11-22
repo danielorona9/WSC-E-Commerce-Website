c6a053d8790a95ebc81e0f88c422a8855240d65a
danielorona9 <dorona@my.devry.edu>
2016-11-21 21:10:05 -0800

    added OrderRequest model class

---
+++ WSC-E-Commerce-Website/Models/OrderRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSC_E_Commerce_Website.Models
+{
+    public class OrderRequest
+    {
+        public int OrderRequestID { get; set; }
+        public int ProductCatalogID { get; set; }
+        public int PurchaseOrdersID { get; set; }
+        public int Quantity { get; set; }
+        public string Content { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual ProductCatalog ProductCatalog { get; set; }
+        public virtual PurchaseOrders PurchaseOrders { get; set; }
+    }
+}
