0aa65ac06423e52ffbc2a190c362e9ee73720fe8
danielorona9 <dorona@my.devry.edu>
2016-11-21 20:10:04 -0800

    added CreditCardType, BillingInfo and Customer Dbsets to context file

--- WSC-E-Commerce-Website/DAL/EcommerceStoreDB_Context.cs
+++ WSC-E-Commerce-Website/DAL/EcommerceStoreDB_Context.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using WSC_E_Commerce_Website.Models;
 
 namespace WSC_E_Commerce_Website.DAL
 {
     public class EcommerceStoreDB_Context
     {
+        public DbSet<CreditCardType> CreditCardType {get; set;}
+        public DbSet<BillingInfo> BillingInfo { get; set; }
+        public DbSet<Customers>Customers { get; set; }
     }
 }
