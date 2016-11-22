fcab96b87243f3ac81eadcc3a7af3643ae63936e
danielorona9 <dorona@my.devry.edu>
2016-11-21 20:55:14 -0800

    Added EmployeeType model class

---
+++ WSC-E-Commerce-Website/Models/EmployeeType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSC_E_Commerce_Website.Models
+{
+    public class EmployeeType
+    {
+        public int EmployeeTypeID { get; set; }
+        public string EmployeeTypeName { get; set; }
+
+        public ICollection<Employee> Employee { get; set; }
+
+    }
+}
