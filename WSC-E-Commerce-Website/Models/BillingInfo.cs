e3a5e9026de85c48f98baa907b1ee6541b281beb
danielorona9 <dorona@my.devry.edu>
2016-11-23 19:46:16 -0800

    added data annotations to BillingInfo model

--- WSC-E-Commerce-Website/Models/BillingInfo.cs
+++ WSC-E-Commerce-Website/Models/BillingInfo.cs
@@ -17,15 +17,17 @@ namespace WSC_E_Commerce_Website.Models
 
         [Required(ErrorMessage = "Enter Credit Card number")]
         [StringLength(16)]
+        [DataType(DataType.CreditCard)]
         [Display(Name = "Credit Card Number")]
         public int CreditNumber { get; set; }
 
-        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Enter expiration date")]
         [DisplayFormat(DataFormatString = "{0:MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         [Display(Name = "Expiration Date")]
         public DateTime ExpirationDate { get; set; }
 
-        //connected to tables that are not one to many or FK
+        //connected to tables that are the one side
         public virtual Customers Customers { get; set; }
         public virtual CreditCardType CreditCardType { get; set; }
     }
