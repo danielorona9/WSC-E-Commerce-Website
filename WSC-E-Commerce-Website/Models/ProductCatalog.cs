22699fda02adc3673880c1433ac3c2c7fd7f6512
danielorona9 <dorona@my.devry.edu>
2016-11-23 20:12:35 -0800

    added data annotations to ProductCatalog

--- WSC-E-Commerce-Website/Models/ProductCatalog.cs
+++ WSC-E-Commerce-Website/Models/ProductCatalog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,13 +9,24 @@ namespace WSC_E_Commerce_Website.Models
     public class ProductCatalog
     {
         public int ProductCatalogID { get; set; }
+
         public int JobTypeID { get; set; }
+
         public int MediaTypeID { get; set; }
+
+        [Display(Name = "Product Price")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
         public int StockAvailable { get; set; }
+
+        [Display(Name = "Image")]
         public string ProductImage { get; set; }
 
+        //connected tables that are on the one side
         public virtual JobTypes JobType { get; set; }
         public virtual MediaTypes MediaType { get; set; }
     }
