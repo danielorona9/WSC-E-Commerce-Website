ba3a6c0dfe3de1b33091da308148b3feb11c3089
danielorona9 <dorona@my.devry.edu>
2016-11-23 20:07:33 -0800

    added comment to orderType model

--- WSC-E-Commerce-Website/Models/OrderType.cs
+++ WSC-E-Commerce-Website/Models/OrderType.cs
@@ -8,8 +8,10 @@ namespace WSC_E_Commerce_Website.Models
     public class OrderType
     {
         public int OrderTypeID { get; set; }
+
         public string OrderTypeName { get; set; }
    
+        //connected tables on the many side
         public virtual ICollection<PurchaseOrders> PurchaseOrders { get; set; }
     }
 }
