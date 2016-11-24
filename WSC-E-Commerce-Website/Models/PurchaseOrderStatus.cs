28f42aa3b5eef5f3ef84e769cbd74acfebfddb9b
danielorona9 <dorona@my.devry.edu>
2016-11-23 20:18:22 -0800

    added comments

--- WSC-E-Commerce-Website/Models/PurchaseOrderStatus.cs
+++ WSC-E-Commerce-Website/Models/PurchaseOrderStatus.cs
@@ -10,6 +10,7 @@ namespace WSC_E_Commerce_Website.Models
         public int PurchaseOrderStatusID { get; set; }
         public string StatusName { get; set; }
 
+        //connected tables on the many side
         public virtual ICollection<PurchaseOrders>PurchaseOrders { get; set; }
     }
 }
