c1ef6841cd928d1c4c59e1fdb156481c92645ba2
danielorona9 <dorona@my.devry.edu>
2016-11-23 20:00:04 -0800

    added some data annotations and comment to media type  model

--- WSC-E-Commerce-Website/Models/MediaTypes.cs
+++ WSC-E-Commerce-Website/Models/MediaTypes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace WSC_E_Commerce_Website.Models
     public class MediaTypes
     {
         public int MediaTypeID { get; set; }
+
+        [Display(Name = "Media Type")]
         public string MediaTypeName { get; set; }
 
+        //connected tables on the many side
         public virtual ICollection<ProductCatalog> ProductCatalog { get; set; }
     }
 }
