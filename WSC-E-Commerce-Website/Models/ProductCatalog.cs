4c4ed7f4c9d6e8afce6539397c00f562743cdfd7
danielorona9 <dorona@my.devry.edu>
2016-11-24 01:34:32 -0800

    added table name , primary key, foreign keys and validation annotations

--- WSC-E-Commerce-Website/Models/ProductCatalog.cs
+++ WSC-E-Commerce-Website/Models/ProductCatalog.cs
@@ -1,23 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
 namespace WSC_E_Commerce_Website.Models
 {
+    [Table("ProductCatalog")]
     public class ProductCatalog
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ProductCatalogID { get; set; }
 
+        [ForeignKey("JobType")]
         public int JobTypeID { get; set; }
 
+        [ForeignKey("MediaType")]
         public int MediaTypeID { get; set; }
 
+        [Required(ErrorMessage = "Enter price of the product")]
         [Display(Name = "Product Price")]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage ="Enter description of the product")]
         [Display(Name = "Description")]
         public string Description { get; set; }
 
