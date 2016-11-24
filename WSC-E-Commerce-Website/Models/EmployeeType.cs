eaa41c4c75504c21cf45b78d4008c47918b02891
danielorona9 <dorona@my.devry.edu>
2016-11-23 19:55:59 -0800

    added data annotations to EmployeeType model

--- WSC-E-Commerce-Website/Models/EmployeeType.cs
+++ WSC-E-Commerce-Website/Models/EmployeeType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,12 @@ namespace WSC_E_Commerce_Website.Models
     public class EmployeeType
     {
         public int EmployeeTypeID { get; set; }
+
+        [Required(ErrorMessage = "Enter employee title")]
+        [Display(Name ="Employee Title")]
         public string EmployeeTypeName { get; set; }
 
+        //connected tables that are on the many side
         public ICollection<Employee> Employee { get; set; }
 
     }
