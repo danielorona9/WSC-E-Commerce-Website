261f2a43ef24b2a510092f60b1815e5e34424eb2
danielorona9 <dorona@my.devry.edu>
2016-11-28 22:53:39 -0800

    changes to show images in the productCatalog view

---
+++ WSC-E-Commerce-Website/CustomHtmlHelpers/CustomHtmlHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WSC_E_Commerce_Website.CustomHtmlHelpers
+{
+    public static class CustomHtmlHelpers
+    {
+        //creates a html helper tag to use @html.image(helper, src, alt)
+        public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+            tb.Attributes.Add("alt", alt);
+            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+
+        }
+    }
+}
