153e3d30d3b129ce323bdeba9b6eec28c1afe32f
danielorona9 <dorona@my.devry.edu>
2016-11-30 22:59:06 -0800

    added catalog products to the home view index and method in the home controller

--- WSC-E-Commerce-Website/Controllers/HomeController.cs
+++ WSC-E-Commerce-Website/Controllers/HomeController.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WSC_E_Commerce_Website.DAL;
 
 namespace WSC_E_Commerce_Website.Controllers
 {
     public class HomeController : Controller
     {
+        private EcommerceStoreDB_Context db = new EcommerceStoreDB_Context();
+
+        // GET: ProductCatalog  
         public ActionResult Index()
-        {
-            return View();
+        {                 
+            {
+                var productCatalog = db.ProductCatalog.Include(p => p.JobType).Include(p => p.MediaType);
+                return View(productCatalog.ToList());
+            }
         }
 
         public ActionResult About()
