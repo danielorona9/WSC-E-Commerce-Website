59b6b1d590aa0ea1c1448bcafba095ed34a384ca
danielorona9 <dorona@my.devry.edu>
2016-12-06 17:42:20 -0800

    added command to allow attribute routing in controller classes

--- WSC-E-Commerce-Website/App_Start/RouteConfig.cs
+++ WSC-E-Commerce-Website/App_Start/RouteConfig.cs
@@ -13,7 +13,9 @@ namespace WSC_E_Commerce_Website
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
+            routes.MapMvcAttributeRoutes();
+
+            routes.MapRoute(           
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
