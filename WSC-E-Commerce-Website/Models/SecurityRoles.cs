0aa22babb2bb20eb35a798ef9bc47374229c5ed2
danielorona9 <dorona@my.devry.edu>
2016-12-14 16:31:55 -0800

    added a class to put all the newly created roles and have intellisence everywhere the roles are applied to.

---
+++ WSC-E-Commerce-Website/Models/SecurityRoles.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSC_E_Commerce_Website.Models
+{
+    public static class SecurityRoles
+    {
+        public const string Admin = "Admin";
+        public const string OperationsManager = "Operations Manager";
+        public const string SalesClerk = "Sales Clerk";
+    }
+}
