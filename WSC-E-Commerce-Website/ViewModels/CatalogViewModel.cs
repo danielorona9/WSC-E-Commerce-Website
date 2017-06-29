c634bf0885389ec152a24160ddd5cbc028291d1f
danielorona9 <dorona@my.devry.edu>
2017-06-29 15:51:09 -0700

    added viewmodel  to bind model with dropdown list

--- WSC-E-Commerce-Website/ViewModels/CatalogViewModel.cs
+++ WSC-E-Commerce-Website/ViewModels/CatalogViewModel.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using WSC_E_Commerce_Website.Models;
 
 namespace WSC_E_Commerce_Website.ViewModels
 {
     public class CatalogViewModel
     {
-        public IEnumerable<ProductCatalog> Items { get; set; }
-       // public string MediaTypeName { get; set; }
-       // public string JobTypeName { get; set; }
+        public List<ProductCatalog> Items { get; set; }
+        public List<MediaTypes> MediaType { get; set; }
+        public List<JobTypes> JobType { get; set; }
+        
+        public int selectMediaTypeId { get; set; }
+        public int selectJobTypeId { get; set; }
     }
 }
